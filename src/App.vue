323ea254ec8b8420af893da279b9a1e3eff314d8
Akifumi1119 <akifumi.doi1119@gmail.com>
2026-07-29 16:45:33 +0900

    20260729 文言修正

--- src/App.vue
+++ src/App.vue
@@ -400,7 +400,7 @@ const checkLottery = () => {
 
     <footer class="footer">
       <p class="footer-text">Front: Vercel Back: Render</p>
-      <p class="footer-text">© {{ new Date().getFullYear() }} 宝くじ当選チェッカー | _1kax</p>
+      <p class="footer-text">© {{ new Date().getFullYear() }} 宝くじ当選チェッカー | Akifumi Doi</p>
     </footer>
   </div>
 </template>
